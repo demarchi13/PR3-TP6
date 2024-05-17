bda8d89b9c0386b82680b7fae5793e6b8def27c9
demarchi13 <lucasdm095@gmail.com>
2024-05-17 01:34:08 -0300

    primera subida

---
+++ TP6_GRUPO_NRO_10/Ejercicio1.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TP6_GRUPO_NRO_10
+{
+    public partial class Ejercicio1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {                       
+                cargarGrillaProductos();
+            }
+        }
+
+        public void cargarGrillaProductos()
+        {
+            GestionProductos objGP = new GestionProductos();
+            grdProductos.DataSource = objGP.ObtenerTodosLosProductos();
+            grdProductos.DataBind();
+        }
+
+        protected void grdProductos_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            grdProductos.PageIndex = e.NewPageIndex;
+            cargarGrillaProductos();
+        }
+
+        protected void grdProductos_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {       
+            try
+            {
+                string idProducto = ((Label)grdProductos.Rows[e.RowIndex].FindControl("lbl_it_IdProducto")).Text;                     
+            
+                Producto objProducto = new Producto();
+                objProducto.IdProducto = Convert.ToInt32(idProducto);
+
+                GestionProductos objGestionProducto = new GestionProductos();
+
+                lblIdProducto.Text = "Id producto seleccionado y eliminado: " + objProducto.IdProducto.ToString();
+                objGestionProducto.EliminarProducto(objProducto);
+                cargarGrillaProductos();
+            }
+            catch (Exception ex)
+            {
+                lblEx.Text = ex.Message;             
+            }
+        }
+
+        protected void grdProductos_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            grdProductos.EditIndex = e.NewEditIndex;
+            cargarGrillaProductos();
+        }
+
+        protected void grdProductos_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            grdProductos.EditIndex = -1;
+            cargarGrillaProductos();
+        }
+
+        protected void grdProductos_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                //Buscar y obtener los datos del edit item template
+                string aux_idProducto = ((Label)grdProductos.Rows[e.RowIndex].FindControl("lbl_eit_idProducto")).Text;
+                string aux_NombreProducto = ((TextBox)grdProductos.Rows[e.RowIndex].FindControl("txt_eid_NombreProducto")).Text;
+                string aux_CantidadPorUnidad = ((TextBox)grdProductos.Rows[e.RowIndex].FindControl("txt_eit_CantidadPorUnidad")).Text;
+                string aux_PrecioUnidad = ((TextBox)grdProductos.Rows[e.RowIndex].FindControl("txt_eit_PrecioUnidad")).Text;
+
+                //Copiar los datos al objeto
+                Producto objProducto = new Producto();
+                objProducto.IdProducto = Convert.ToInt32(aux_idProducto);
+                objProducto.NombreProducto = aux_NombreProducto;
+                objProducto.CantidadPorUnidad = aux_CantidadPorUnidad;
+                objProducto.PrecioUnidad = Convert.ToDouble(aux_PrecioUnidad);
+
+                //Declara obj gestion productos y llama al metodo
+                GestionProductos objGestionProductos = new GestionProductos();
+                if (objGestionProductos.ActualizarProducto(objProducto))
+                {
+                    lblIdProducto.Text = "Producto actualizado con exito";
+                }
+                else
+                {
+                    lblIdProducto.Text = "No se pudo actualizar el producto";
+                }
+
+                //Salir del modo edicion
+                grdProductos.EditIndex = -1;
+
+                //Actualiza la grilla
+                cargarGrillaProductos();
+            }
+            catch (Exception ex)
+            {
+                lblEx.Text = ex.Message;
+            }
+        }
+    }
+}
---
+++ TP6_GRUPO_NRO_10/Inicio.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Inicio.aspx.cs" Inherits="TP6_GRUPO_NRO_10.Inicio" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: xx-large;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <strong><span class="auto-style1">Grupo Nº 10</span></strong><br />
+            <br />
+            <br />
+            <br />
+            <asp:HyperLink ID="hypEjercicio1" runat="server" NavigateUrl="~/Ejercicio1.aspx">Ejercicio 1</asp:HyperLink>
+            <br />
+            <br />
+            <br />
+            <asp:HyperLink ID="hypEjercicio2" runat="server" NavigateUrl="~/Ejercicio2.aspx">Ejercicio 2</asp:HyperLink>
+        </div>
+    </form>
+</body>
+</html>
